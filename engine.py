ba2719c533ba95e7d6d44128cc5c22df41743cb8
Matthew Speight <matthew1270.ms@gmail.com>
2019-05-26 19:53:04 +0100

    Working on chromium-browser

--- engine.py
+++ engine.py
@@ -53,7 +53,7 @@ def getVid(movieList): # MovieList Array
 	for x in range(len(movieList)):
 		print(str(x+1)+" - "+ movieNames[x])
 
-	movieId = int(raw_input("Which film to watch: (0-"+str(len(movieList))+")"))-1
+	movieId = int(raw_input("Which film to watch: (1-"+str(len(movieList))+")"))-1
 	movieurl = movieList[movieId]
 	
 	headers={"Host":"123movies00.org","User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language":"en-GB,en;q=0.5","Accept-Encoding":"gzip, deflate","Connection":"close","Cookie":"__test; noShowWelcome=true; __test; __cfduid=d406039e696df40844f87a00d7e8453191558888545; __atuvc=4%7C22; __atuvs=5ceac062b397b565003; _ga=GA1.2.1128633050.1558888547; _gid=GA1.2.2048887501.1558888547; subscribe=1; PHPSESSID=kanhb6f5ddcr5e7rs0q02n1ig7; AdskeeperStorage=%7B%220%22%3A%7B%22svspr%22%3A%22https%3A%2F%2F123movies00.org%2Fmovie%2Fsearch%2Ftransporter%22%2C%22svsds%22%3A1%2C%22TejndEEDj%22%3A%22TyvYeYhpV%22%7D%2C%22C344312%22%3A%7B%22page%22%3A1%2C%22time%22%3A1558888562274%7D%7D","Upgrade-Insecure-Requests":"1"}
