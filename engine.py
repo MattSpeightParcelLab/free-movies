9b8152442195d528ccb06b14a3b700f0336ba5da
Matthew Speight <matthew1270.ms@gmail.com>
2019-05-26 20:40:16 +0100

    Print vid URL

--- engine.py
+++ engine.py
@@ -10,11 +10,53 @@ GET /movie/search/transporter HTTP/1.1
 """
 
 
-def watchFilm(): #By Name
-	searchTerm = raw_input("Film Name: ") # Update code for python3
+def watchFilm(searchTerm): #By Name
 	headers={"Host":"123movies00.org","User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language":"en-GB,en;q=0.5","Accept-Encoding":"gzip, deflate","Connection":"close","Cookie":"__test; noShowWelcome=true; __test; __cfduid=d406039e696df40844f87a00d7e8453191558888545; __atuvc=4%7C22; __atuvs=5ceac062b397b565003; _ga=GA1.2.1128633050.1558888547; _gid=GA1.2.2048887501.1558888547; subscribe=1; PHPSESSID=kanhb6f5ddcr5e7rs0q02n1ig7; AdskeeperStorage=%7B%220%22%3A%7B%22svspr%22%3A%22https%3A%2F%2F123movies00.org%2Fmovie%2Fsearch%2Ftransporter%22%2C%22svsds%22%3A1%2C%22TejndEEDj%22%3A%22TyvYeYhpV%22%7D%2C%22C344312%22%3A%7B%22page%22%3A1%2C%22time%22%3A1558888562274%7D%7D","Upgrade-Insecure-Requests":"1"}
 	#cookies = {"__test":"","noShowWelcome":"true","__test":"","__cfduid":"d406039e696df40844f87a00d7e8453191558888545","__atuvc":"3%7C22","__atuvs":"5ceac062b397b565002","_ga":"GA1.2.1128633050.1558888547","_gid":"GA1.2.2048887501.1558888547","subscribe":"1","PHPSESSID":"kanhb6f5ddcr5e7rs0q02n1ig7","AdskeeperStorage":"%7B%220%22%3A%7B%22svspr%22%3A%22https%3A%2F%2F123movies00.org%2Fmovie%2Fsearch%2Ftransporter%22%2C%22svsds%22%3A1%2C%22TejndEEDj%22%3A%22TyvYeYhpV%22%7D%2C%22C344312%22%3A%7B%22page%22%3A1%2C%22time%22%3A1558888562274%7D%7D"}
 	r = requests.post("https://123movies00.org/movie/search/"+searchTerm, headers=headers).text
+
+	tempurls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', r)
+	
+	urls = []
+
+	for url in tempurls:
+		url = url.encode('ascii','ignore').lower()
+		if searchTerm in url:
+				urls.append(url)
+
+	#tempurls = checkFilm(urls)
+	urls = []
+
+	for url in tempurls:
+		url = url.encode('ascii','ignore').lower()
+		if searchTerm in url:
+			if "https://123movies00.org/movie/" in url:
+				if ".html" in url:
+					urls.append(url)
+	urls = urls #+ tempurls
+
+	getVid(urls)
+
+"""	
+	extra = []
+	for i in range(len(tempurls)):
+		for x in range(len(urls)):
+			print urls[x]
+			if urls[x] in tempurls[i]:
+				print tempurls[i]
+			else:
+				extra.append(tempurls[i])
+				print tempurls[i]
+"""			
+
+
+
+def checkFilm(urls): #By Name
+	headers={"Host":"123movies00.org","User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language":"en-GB,en;q=0.5","Accept-Encoding":"gzip, deflate","Connection":"close","Cookie":"__test; noShowWelcome=true; __test; __cfduid=d406039e696df40844f87a00d7e8453191558888545; __atuvc=4%7C22; __atuvs=5ceac062b397b565003; _ga=GA1.2.1128633050.1558888547; _gid=GA1.2.2048887501.1558888547; subscribe=1; PHPSESSID=kanhb6f5ddcr5e7rs0q02n1ig7; AdskeeperStorage=%7B%220%22%3A%7B%22svspr%22%3A%22https%3A%2F%2F123movies00.org%2Fmovie%2Fsearch%2Ftransporter%22%2C%22svsds%22%3A1%2C%22TejndEEDj%22%3A%22TyvYeYhpV%22%7D%2C%22C344312%22%3A%7B%22page%22%3A1%2C%22time%22%3A1558888562274%7D%7D","Upgrade-Insecure-Requests":"1"}
+	#cookies = {"__test":"","noShowWelcome":"true","__test":"","__cfduid":"d406039e696df40844f87a00d7e8453191558888545","__atuvc":"3%7C22","__atuvs":"5ceac062b397b565002","_ga":"GA1.2.1128633050.1558888547","_gid":"GA1.2.2048887501.1558888547","subscribe":"1","PHPSESSID":"kanhb6f5ddcr5e7rs0q02n1ig7","AdskeeperStorage":"%7B%220%22%3A%7B%22svspr%22%3A%22https%3A%2F%2F123movies00.org%2Fmovie%2Fsearch%2Ftransporter%22%2C%22svsds%22%3A1%2C%22TejndEEDj%22%3A%22TyvYeYhpV%22%7D%2C%22C344312%22%3A%7B%22page%22%3A1%2C%22time%22%3A1558888562274%7D%7D"}
+	r = ""
+	for i in range(len(urls)):
+		r += requests.post(urls[i], headers=headers).text
 
 	tempurls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', r)
 
@@ -29,7 +71,7 @@ def watchFilm(): #By Name
 					urls.append(url)
 
 	
-	getVid(urls)
+	return urls
 
 
 
@@ -37,16 +79,7 @@ def watchFilm(): #By Name
 def getVid(movieList): # MovieList Array
 	#Choose Film
 
-	movieNames = []
-
-	for movie in movieList:
-
-		movie = find(movie, ["e", "1"])
-		movie = find(movie, ["e", "1"])
-		movie = find(movie, ["/", "/"])
-		movie = find(movie, ["/", "/"])
-
-		movieNames.append(movie)
+	movieNames = getMoveNames(movieList)
 
 		#movieNames.append(find(find(find(find(movie, ["e", "1"]), ["e", "1"]), ["\/", "\/"]), ["\/", "\/"])) # get video id
 
@@ -67,6 +100,21 @@ def getVid(movieList): # MovieList Array
 	except:
 		print("https://openload.co/embed/"+id)
 
+
+def getMoveNames(movieList):
+
+	movieNames = []
+
+	for movie in movieList:
+
+		movie = find(movie, ["e", "1"])
+		movie = find(movie, ["e", "1"])
+		movie = find(movie, ["/", "/"])
+		movie = find(movie, ["/", "/"])
+
+		movieNames.append(movie)
+
+	return movieNames
 
 	# Search
 def search(txt, searchvar):
@@ -102,8 +150,8 @@ def find(txt, var):
     return text
 
  
-
-watchFilm()
+searchTerm = raw_input("Film Name: ") # Update code for python3
+watchFilm(searchTerm)
 
 
 
