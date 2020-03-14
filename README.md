5bd6b60feecb3b8a06e3e078b322f2b148c7547a
Matt Speight <matthew1270.ms@gmail.com>
2020-03-14 12:14:47 +0000

    ---XXXXX---

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# free-movies
+# Movies - Free without ads (Stored on 3rd party sites)
 
 Easy to search for a film - Little to no ads
+
+    1) Movie loads with browser
+    2) Movie URL displayed in console
--- engine.py
+++ engine.py
@@ -13,7 +13,7 @@ GET /movie/search/transporter HTTP/1.1
 def watchFilm(searchTerm): #By Name
 	headers={"Host":"123movies00.org","User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0","Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language":"en-GB,en;q=0.5","Accept-Encoding":"gzip, deflate","Connection":"close","Cookie":"__test; noShowWelcome=true; __test; __cfduid=d406039e696df40844f87a00d7e8453191558888545; __atuvc=4%7C22; __atuvs=5ceac062b397b565003; _ga=GA1.2.1128633050.1558888547; _gid=GA1.2.2048887501.1558888547; subscribe=1; PHPSESSID=kanhb6f5ddcr5e7rs0q02n1ig7; AdskeeperStorage=%7B%220%22%3A%7B%22svspr%22%3A%22https%3A%2F%2F123movies00.org%2Fmovie%2Fsearch%2Ftransporter%22%2C%22svsds%22%3A1%2C%22TejndEEDj%22%3A%22TyvYeYhpV%22%7D%2C%22C344312%22%3A%7B%22page%22%3A1%2C%22time%22%3A1558888562274%7D%7D","Upgrade-Insecure-Requests":"1"}
 	#cookies = {"__test":"","noShowWelcome":"true","__test":"","__cfduid":"d406039e696df40844f87a00d7e8453191558888545","__atuvc":"3%7C22","__atuvs":"5ceac062b397b565002","_ga":"GA1.2.1128633050.1558888547","_gid":"GA1.2.2048887501.1558888547","subscribe":"1","PHPSESSID":"kanhb6f5ddcr5e7rs0q02n1ig7","AdskeeperStorage":"%7B%220%22%3A%7B%22svspr%22%3A%22https%3A%2F%2F123movies00.org%2Fmovie%2Fsearch%2Ftransporter%22%2C%22svsds%22%3A1%2C%22TejndEEDj%22%3A%22TyvYeYhpV%22%7D%2C%22C344312%22%3A%7B%22page%22%3A1%2C%22time%22%3A1558888562274%7D%7D"}
-	r = requests.post("https://123movies00.org/movie/search/"+searchTerm, headers=headers).text
+	r = requests.post("https://www2.123movies11.com/movie/search/"+searchTerm, headers=headers).text
 
 	tempurls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', r)
 	
@@ -30,7 +30,7 @@ def watchFilm(searchTerm): #By Name
 	for url in tempurls:
 		url = url.encode('ascii','ignore').lower()
 		if searchTerm in url:
-			if "https://123movies00.org/movie/" in url:
+			if "https://www2.123movies11.com/movie/" in url:
 				if ".html" in url:
 					urls.append(url)
 	urls = urls #+ tempurls
@@ -66,7 +66,7 @@ def checkFilm(urls): #By Name
 
 		url = url.encode('ascii','ignore').lower()
 		if searchTerm in url:
-			if "https://123movies00.org/movie/" in url:
+			if "https://www2.123movies11.com/movie/" in url:
 				if ".html" in url:
 					urls.append(url)
 
